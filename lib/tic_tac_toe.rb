3d749f6e7b680e06f3b4ed17aa0cab03dda9538c
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-04 20:00:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -147,11 +147,5 @@ def play(board)
   while !over?(board)
     turn(board)
   end
-  if won?(board)
-    puts "Congratulations " + winner(board) + "!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  else
-    #do nothing
-  end
+  
 end
